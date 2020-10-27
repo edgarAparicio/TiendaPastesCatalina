e0baeec4d17973925b7b9d5c3280c7e5b63654a7
Edgar Guillermo Aparicio Haro <edgar_chato@live.com.mx>
2020-10-27 09:43:41 -0600

    Se otra migracion por cambios en las entidades

---
+++ DataPastesCatalina/Migrations/20201027045617_MigracionBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EdgarAparicio.PastesCatalina.Data.Migrations
+{
+    public partial class MigracionBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
